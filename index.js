4ec755bd2a963e4b6dd6987d230f3e58096f2ccc
Jose Emilio Antonio Jimenez <a369664@alumnos.uaslp.mx>
2024-09-15 19:41:56 -0600

    nav bar
    se cambio el estilo del nav bar a uno estilo hamburguesa modo responsive

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-$(window).load(function(){
-    $('#f1').flexslider({
-      animation: "slide",
-      rtl: true,
-      start: function(slider){
-        $('body').removeClass('loading');
-      }
-    });
-   $('#f2').flexslider({
-      animation: "slide",
-      animationLoop: false,
-      itemWidth: 210,
-      itemMargin: 5,
-      pausePlay: true,
-      mousewheel: true,
-      rtl: true,
-      asNavFor:'.flexslider'
-    });
-  });
+document.getElementById('toggleIcon').addEventListener('click', function() {
+  var navBar = document.querySelector('.responsive-bar');
+  if (navBar.classList.contains('show')) {
+      navBar.classList.remove('show');
+      navBar.classList.add('hide');
+      setTimeout(function() {
+          navBar.style.display = 'none';
+          navBar.classList.remove('hide');
+      }, 600); // Tiempo de la transición
+  } else {
+      navBar.style.display = 'block';
+      setTimeout(function() {
+          navBar.classList.add('show');
+      }, 10); // Pequeño retraso para permitir que el display se aplique
+  }
+});
---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Servir archivos estáticos desde el directorio raíz
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
